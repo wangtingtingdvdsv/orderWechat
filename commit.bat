8d8bf3dca0412a0205a8086dcac0e752b7048089
wangtingting <2372454518@qq.com>
2018-12-04 13:39:51 +0800

    持续更新中

--- commit.bat
+++ commit.bat
@@ -4,8 +4,8 @@ echo "*************************"
 echo git commit -m "持续更新中"
 git commit -m "持续更新中"
 echo "***************************"
-echo git push origin master
-git push origin master
+echo git push origin develop
+git push origin develop
 echo "***************************"
 echo git status
 git status
